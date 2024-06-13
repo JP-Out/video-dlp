432b3934c78af829d71b92ce559a7dc7575fb7b9
JP-Out <joaopedroscheffler@gmail.com>
2024-06-13 18:00:12 -0300

    REGEX Format for popup percentage

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,21 +1,27 @@
 async function getProgress() {
-    try {
+      try {
         const response = await fetch('http://localhost:5000/progress', {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            }
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json'
+          }
         });
         if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not ok ' + response.statusText);
         }
         const data = await response.json();
-        document.getElementById('progress').innerText = data.percent + '%';
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-    }
-}
 
-// Update progress every second
-setInterval(getProgress, 1000);
+        const rawPercent = data.percent;
+        debugger
+        const percent = rawPercent.replace(/\x1b\[[0-9;]*m/g, '').trim(); // [0;94m 6.6[0m%
+
+        // Formatar percentagem e exibir
+        document.getElementById('progress').innerText = percent;
+      } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+      }
+    }
+
+    // Update progress every second
+    setInterval(getProgress, 1000);
